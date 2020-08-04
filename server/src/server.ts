a9cbc319204cd47d5face9daf335adb410d31ee3
Allan Galdino <allan.g.galdino@outlook.com>
2020-08-04 17:29:13 -0300

    create a class done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
