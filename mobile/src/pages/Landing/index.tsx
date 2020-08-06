c997e0ffa055459641f9d565b21e79bada048cc6
Allan Galdino <allan.g.galdino@outlook.com>
2020-08-06 14:16:35 -0300

    landing page complete and navigation configured

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 import landingImage from '../../assets/images/landing.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import giveClassesIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
 
 function Landing() {
+  const { navigate } = useNavigation();
+
+  const handleNavigateToGiveClassesPage = useCallback(() => {
+    navigate('GiveClasses');
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -13,6 +24,26 @@ function Landing() {
       <Text style={styles.title}>
         Seja bem-vindo, {'\n'}
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Estudar</Text>
+        </RectButton>
+
+        <RectButton
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
+          <Image source={giveClassesIcon} />
+          <Text style={styles.buttonText}>Dar aula</Text>
+        </RectButton>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        Total de XX conexoes já realizadas{' '}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
