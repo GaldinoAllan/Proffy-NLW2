e8ae3068477ea55f1f6f548a8c7baabb512b4814
Allan Galdino <allan.g.galdino@outlook.com>
2020-08-07 17:08:44 -0300

    Teacher Item component done

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
 
@@ -8,6 +10,22 @@ function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title='Proffys Disponíveis' />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
